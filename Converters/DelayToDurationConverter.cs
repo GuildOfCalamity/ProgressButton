2e249d2a9be1423161cc0eeb5101a553c470ff2c
GuildOfCalamity <81915293+GuildOfCalamity@users.noreply.github.com>
2023-09-26 17:21:07 -0400

    uploaded supporting folders

---
+++ Converters/DelayToDurationConverter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgressButtonDemo;
+
+public class DelayToDurationConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        double factor = 0.5;
+
+        if (!Enum.IsDefined(typeof(DelayTime), value))
+            throw new ArgumentException("DelayToDurationConverter: value must be an enum");
+
+        if (parameter != null && parameter is string optional)
+        {
+            // You should adjust the factor based on your enum values.
+            if (double.TryParse(optional, out var amount))
+            {
+                System.Diagnostics.Debug.WriteLine($"Detected parameter: {amount}");
+                factor = amount;
+            }
+        }
+
+        try
+        {
+            var enumValue = (DelayTime)Enum.Parse(typeof(DelayTime), $"{value}");
+            return new Duration(TimeSpan.FromSeconds(factor * (int)enumValue));
+        }
+        catch (Exception ex)
+        {
+            System.Diagnostics.Debug.WriteLine($"DelayToDurationConverter: {ex.Message}");
+            return new Duration(TimeSpan.FromSeconds(1));
+        }
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        return null;
+    }
+}
---
+++ Converters/EnumToBooleanConverter.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgressButtonDemo;
+
+public enum DelayTime
+{
+    None = 0,
+    Short = 1,
+    Medium = 3,
+    Long = 5
+}
+
+public class EnumToBooleanConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        if (parameter != null && parameter is string enumString)
+        {
+            if (!Enum.IsDefined(typeof(DelayTime), value))
+            {
+                throw new ArgumentException("EnumToBooleanConverter: value must be an enum");
+            }
+            var enumValue = Enum.Parse(typeof(DelayTime), enumString);
+            return enumValue.Equals(value);
+        }
+        System.Diagnostics.Debug.WriteLine($"'{nameof(parameter)}' was empty, nothing to do.");
+        return false;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        if (parameter is string enumString)
+        {
+            return Enum.Parse(typeof(ElementTheme), enumString);
+        }
+        throw new ArgumentException("EnumToBooleanConverter: parameter must be an enum name");
+    }
+}
